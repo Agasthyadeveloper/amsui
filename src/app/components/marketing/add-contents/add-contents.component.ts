87f016f467c723506ca6e2606f23529c6b71e2bb
Agasthyadeveloper <it@agasthyagroup.com>
2024-10-03 17:47:17 +0530

    marketing-dynamic-module

--- src/app/components/marketing/add-contents/add-contents.component.ts
+++ src/app/components/marketing/add-contents/add-contents.component.ts
@@ -6,9 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './add-contents.component.css'
 })
 export class AddContentsComponent {
+
+  // side bar
+
   isSidebarOpen = true;
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
+
+  // end side bar
+  selectedModule: string = '';
+  modules: string[] = ['Content Tracker', 'Collateral Tracker','Marketing Calender'];
 }
