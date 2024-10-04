48348a0815db5497ab6c3035c9c7220b11627766
Agasthyadeveloper <it@agasthyagroup.com>
2024-10-04 16:37:38 +0530

    status-task-marketing

--- src/app/components/marketing/content-status/content-status.component.ts
+++ src/app/components/marketing/content-status/content-status.component.ts
@@ -11,4 +11,32 @@ export class ContentStatusComponent {
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
+
+  selectedModule: string = '';
+  modules: string[] = ['Content Tracker', 'Collateral Tracker', 'Marketing Calender'];
+
+  downloadCSV() {
+    const table = document.querySelector('table');
+    const rows = table?.querySelectorAll('tr');
+    let csvContent = '';
+  
+    rows?.forEach(row => {
+      const cols = row.querySelectorAll('th, td');
+      const csvRow: string[] = [];  // Explicitly define csvRow as a string array
+      cols.forEach(col => {
+        // Get text content and escape quotes
+        const cellText = col.textContent?.trim().replace(/"/g, '""');
+        csvRow.push(`"${cellText}"`);
+      });
+      csvContent += csvRow.join(',') + '\n';
+    });
+  
+    // Trigger CSV download
+    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'table_data.csv';
+    link.click();
+  }
+  
 }
--- src/app/components/marketing/update-content/update-content.component.ts
+++ src/app/components/marketing/update-content/update-content.component.ts
@@ -11,4 +11,36 @@ export class UpdateContentComponent {
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
+
+  selectedModule: string = '';
+  modules: string[] = ['Content Tracker', 'Collateral Tracker','Marketing Calender'];
+
+  startDate: string = '';
+  endDate: string = '';
+  minEndDate: string = '';
+  numberOfDaysText: string = '0 day';
+
+  updateEndDateLimit() {
+    if (this.startDate) {
+      this.minEndDate = this.startDate;
+      this.calculateNumberOfDays();
+    }
+  }
+
+  calculateNumberOfDays() {
+    if (this.startDate && this.endDate) {
+      const start = new Date(this.startDate);
+      const end = new Date(this.endDate);
+
+      const timeDiff = end.getTime() - start.getTime();
+      const numberOfDays = timeDiff >= 0 ? Math.ceil(timeDiff / (1000 * 3600 * 24)) + 1 : 0;
+      if (numberOfDays === 1) {
+        this.numberOfDaysText = `${numberOfDays} day`;
+      } else {
+        this.numberOfDaysText = `${numberOfDays} days`;
+      }
+    } else {
+      this.numberOfDaysText = '0 day';
+    }
+  }
 }
